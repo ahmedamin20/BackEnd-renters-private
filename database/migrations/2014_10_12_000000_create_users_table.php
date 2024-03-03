7bfbd1259892aad1bf6a007c40582c299a95496f
Mohamed Attar <mohammedattar0100020@gmail.com>
2024-03-03 22:47:38 +0200

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
             $table->rememberToken();
             $table->boolean('status');
             $table->enum('type', \Modules\Auth\Enums\UserTypeEnum::availableTypes());
